b93e3745786256e0533cd85a63106ded1a41692e
Adam Merritt <amerritt3@bloomberg.com>
2021-12-07 11:52:57 -0500

    whatever

--- packages/problems/common/util.ts
+++ packages/problems/common/util.ts
@@ -16,12 +16,12 @@ export async function getInput(solutionDir: string, day: number) {
     try { 
         return `${await readFile(resolve(solutionDir, "input.txt"))}`.trim().split(/\r\n?/).map(val => val.trim())
     } catch (e) {        
-        const response = await fetch(`http://adventofcode.com/2021/day/${day}/input`, {
+        const response = await fetch(`https://adventofcode.com/2021/day/${day}/input`, {
             method: 'GET',
             headers: {
                 'Accept': 'text/html',
                 'Accept-Encoding': 'gzip, deflate, br',
-                Cookie: '_ga=GA1.2.2143524650.1638378988; session=53616c7465645f5f4a86b1a166e37d9359ede92f8429706693c230d52545860e998bdaea983e12dfd1af8567bab438fa; _gid=GA1.2.604389420.1638733352',
+                // Cookie: '_ga=GA1.2.2143524650.1638378988; session=53616c7465645f5f4a86b1a166e37d9359ede92f8429706693c230d52545860e998bdaea983e12dfd1af8567bab438fa; _gid=GA1.2.604389420.1638733352',
                 session: '53616c7465645f5f4a86b1a166e37d9359ede92f8429706693c230d52545860e998bdaea983e12dfd1af8567bab438fa',
                 _ga: 'GA1.2.2143524650.1638378988',
                 _gid: 'GA1.2.604389420.1638733352',
